4a9f0929ce87d979814d38d70ba1d88fc1ff678a
Martin Valle <mdvh05@gmail.com>
2025-10-16 23:54:32 -0400

    feat: enhance ModalSendChangePaid with validation indicators and improved layout

--- src/features/orders/Manual/components/ModalSendChangePaid.tsx
+++ src/features/orders/Manual/components/ModalSendChangePaid.tsx
@@ -1,9 +1,8 @@
-import { InputErrorMessage } from "@/shared/component/InputErrorMessage";
 import { ChangePaidRequest, ManualOrder } from "@/shared/types/dto/orders.dto";
 import {
+  Alert,
   Button,
   createTheme,
-  HelperText,
   Label,
   Modal,
   ModalBody,
@@ -11,7 +10,6 @@ import {
   Spinner,
   TabItem,
   Tabs,
-  TextInput,
   ThemeProvider,
 } from "flowbite-react";
 import { SubmitHandler, useForm } from "react-hook-form";
@@ -21,9 +19,10 @@ import { useCallback, useEffect, useState } from "react";
 import { ordersService } from "@/shared/services/orders.service";
 import { useBCVTasaStore } from "@/app/store/bcv";
 import { currencyFormat } from "@/shared/utils/helpers";
-import { FaArrowsRotate } from "react-icons/fa6";
+import { FaArrowsRotate, FaCheck, FaXmark } from "react-icons/fa6";
 import { PaymentUserData } from "./PaymentUserData";
 import { useAuthStore } from "@/app/store/auth";
+import { HiInformationCircle } from "react-icons/hi";
 
 interface ModalSendChangePaidProps {
   openModal: boolean;
@@ -75,27 +74,9 @@ export const ModalSendChangePaid = ({
     [onResult]
   );
 
-  const {
-    handleSubmit,
-    reset,
-    setValue,
-    watch,
-    formState: { errors },
-  } = useForm<ChangePaidRequest>({
+  const { handleSubmit, reset, watch } = useForm<ChangePaidRequest>({
     resolver: yupResolver(sendChangePaidSchema),
   });
-
-  const handleChangeAmount = useCallback(
-    (value: string) => {
-      const numericValueUSD = parseFloat(value).toFixed(2) as unknown as number;
-      const numericVES = parseFloat(
-        ((numericValueUSD - order!.orderTotalAmount) * tasa.amount).toFixed(2)
-      );
-      setValue("amount", numericVES);
-      setAmount(numericValueUSD);
-    },
-    [setValue, tasa, order]
-  );
 
   useEffect(() => {
     if (openModal && order) {
@@ -121,55 +102,48 @@ export const ModalSendChangePaid = ({
             ) : (
               <Tabs variant="fullWidth">
                 <TabItem active={true} title="Vuelto">
-                  <div className="text-gray-400 text-sm">
+                  <div className="text-sm">
                     <p>
                       {`Monto Total de la Orden: $${
                         order?.orderTotalAmount || 0
                       }`}
                     </p>
+
+                    <p className="flex gap-2 items-center">
+                      {`Validado por logística:`}{" "}
+                      {order?.logisticValidate ? (
+                        <FaCheck color="green" />
+                      ) : (
+                        <FaXmark color="red" />
+                      )}
+                    </p>
+                    <p>Monto cancelado por el usuario: ${amount}</p>
                     <p>{`Tasa del dia: ${currencyFormat.format(
                       tasa.amount
                     )}`}</p>
                   </div>
-                  <div className="flex flex-col gap-2">
-                    <div>
-                      <div className="block">
-                        <Label htmlFor="serial">
-                          Monto cancelado por el usuario (USD):
-                        </Label>
-                      </div>
-                      <TextInput
-                        required
-                        inputMode="numeric"
-                        type="number"
-                        step={0.01}
-                        min={1}
-                        value={amount}
-                        onChange={(e) => handleChangeAmount(e.target.value)}
-                        color={errors.amount && "failure"}
-                      />
-                      <InputErrorMessage message={errors.amount?.message} />
-                    </div>
-                    <div className="mt-1">
-                      <div className="flex justify-between items-center text-sm">
-                        <p className="">
-                          Vuelto:{" "}
-                          {`$${(
-                            amount - (order?.orderTotalAmount || 0)
-                          ).toFixed(2)}`}
-                        </p>
-                        <FaArrowsRotate color="green" />
-                        <Label htmlFor="serial">
-                          Vuelto: {currencyFormat.format(watch("amount"))}
-                        </Label>
-                      </div>
-                      <HelperText>
-                        Al confirmar se enviara el dinero al cliente via Pago
-                        Movil R4. y no podra enviar otro para esta ordern
-                      </HelperText>
+
+                  <div className="flex flex-col gap-2 my-2 bg-bone-light-beige rounded-md">
+                    <div className="flex justify-between items-center text-sm">
+                      <p className="">
+                        Vuelto:{" "}
+                        {`$${(amount - (order?.orderTotalAmount || 0)).toFixed(
+                          2
+                        )}`}
+                      </p>
+                      <FaArrowsRotate color="green" />
+                      <Label htmlFor="serial">
+                        Vuelto: {currencyFormat.format(watch("amount"))}
+                      </Label>
                     </div>
                   </div>
-
+                  <Alert
+                    className="bg-bone-beige text-bone-primary text-xxs py-2 px-4"
+                    icon={HiInformationCircle}
+                  >
+                    Al confirmar se enviara el dinero al cliente via Pago Movil
+                    R4. y no podra enviar otro para esta ordern
+                  </Alert>
                   <div className="flex justify-center mt-2">
                     <Button
                       type="submit"
